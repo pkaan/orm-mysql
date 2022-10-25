3d3c292ae75929e49242e8aff2a928bf347f5ca4
Peetu Kaan <aripeetu.kaan@gmail.com>
2022-10-25 16:49:54 +0100

    refactoring

--- database.js
+++ database.js
@@ -6,6 +6,7 @@ let pass = password.password;
 let pass = "yourpassword"
 const Sequelize = require('sequelize');
 
+
 // CONNECT TO THE DATABASE
 const connection = new Sequelize('bookings_db', 'root', `${pass}`, {
     host: 'localhost', 
@@ -13,15 +14,14 @@ const connection = new Sequelize('bookings_db', 'root', `${pass}`, {
 });
 
 connection
-    .authenticate()
+    .authenticate() // authenticate is used to check whether you can connect to the database without executing a real query
     .then(() => {console.log('Connection to database established succesfully.');
 })
 .catch(err => {
     console.error('Unable to connect to the database:', err);
 })
  
-
-// CREATE TABLE users
+// CREATE TABLE users using MODEL User
 const User = 
     connection.define('User', {
         user_id: {
@@ -31,28 +31,19 @@ const User =
         },
         name: Sequelize.STRING,
         destination: Sequelize.STRING,
-        //timestamps: false
+}, {
+    timestamps: true
 })
 
 // INSERT INTO users (name, destination) VALUES (....)
-console.log("QUERY 1: INSERT INTO ..");
-connection.sync()
-.then(() => User.bulkCreate([{
-    name: "Jason",
-    destination: "Las Palmas"},
-    {
-    name: "Peetu",
-    destination: "Birmingham"},
-    {
-    name: "Maliha",
-    destination: "New York"},
-    {
-    name: "Amita",
-    destination: "Paris"}
-    ])
-).catch(err => {
-    console.error('Error inserting values into table:', err);
-});
+let insertValues = function (data) {
+    console.log("QUERY: INSERT INTO ..");
+    connection.sync()
+    .then(() => User.bulkCreate(data)
+    ).catch(err => {
+        console.error('Error inserting values into table:', err);
+    });
+}
 
 /*
 Using UUID as a primaryKey
@@ -64,32 +55,60 @@ Using UUID as a primaryKey
 */
 
 // FIND ALL, SELECT * FROM ..
+let findall = function () {
 connection.sync().then(() => User.findAll())
 .then(user => {
-    console.log("\nQUERY 2: SELECT * FROM ..");
+    console.log("\nQUERY: SELECT * FROM ..");
     console.log(JSON.stringify(user));
 }).catch(err => {
     console.error('Error getting values from the table:', err);
 })
+}
 
 // FIND BY CONDITION, SELECT * FROM .. WHERE
+let findByCondition = function (value) {
 connection.sync().then(() => User.findAll({
     where: {
-        destination: 'New York'
+        'destination' : `${value}`
     }
 }))
 .then(user => {
-    console.log("\nQUERY 3: SELECT * FROM .. WHERE ");
+    console.log("\nQUERY: SELECT * FROM .. WHERE ");
     console.log(JSON.stringify(user));
 }).catch(err => {
     console.error('Error getting values from the table:', err);
 })
+}
 
 // FIND BY PRIMARY KEY 
-connection.sync().then(() => User.findByPk(1))
+let findByPrimaryKey = function (id) {
+connection.sync().then(() => User.findByPk(id))
 .then(user => {
-    console.log("\nQUERY 4: FIND BY PRIMARY KEY");
-    console.log(JSON.stringify(user));
+    console.log("\nQUERY: FIND BY PRIMARY KEY");
+    console.log(JSON.stringify(user.name));
 }).catch(err => {
     console.error('Error getting values from the table:', err);
 })
+}
+
+const data = [{
+    'name': 'Jason',
+    'destination': 'Las Palmas'},
+    {
+    'name' : 'Peetu',
+    'destination' : "Birmingham"},
+    {
+    'name': 'Maliha',
+    'destination' : 'New York'},
+    {
+    'name' : 'Amita',
+    'destination' : 'Paris'}
+]
+
+insertValues(data);
+
+findall();
+
+findByCondition("new york");
+
+findByPrimaryKey(1); // Should print 'Jason'
